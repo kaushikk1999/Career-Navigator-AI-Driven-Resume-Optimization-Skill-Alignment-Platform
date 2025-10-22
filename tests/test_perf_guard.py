8d868dd9d7b885c5215083014a9ba38f82f52144
kaushik karmakar <kaushikka99@gmal.com>
2025-10-22 20:12:48 +0530

    2

--- tests/test_perf_guard.py
+++
@@ -1,8 +0,0 @@
-from phase1 import ocr
-
-
-def test_should_retry_thresholds():
-    assert ocr._should_retry(4, 65.0) is True
-    assert ocr._should_retry(6, 69.9) is True
-    assert ocr._should_retry(4, 80.0) is False
-    assert ocr._should_retry(11, 40.0) is False
